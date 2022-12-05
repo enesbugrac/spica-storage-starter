1bbb4543ad48a2f2fa0c13b82ce0eb054eea4999
enesbugrac <enesbc46@gmail.com>
2022-12-05 15:27:08 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Modal, { ModalBody, ModalFooter, ModalHeader } from "./components/Modal";
+
+const products = [
+  {
+    _id: "1",
+    product_name: "Apple",
+  },
+  {
+    _id: "1",
+    product_name: "Orange",
+  },
+  {
+    _id: "1",
+    product_name: "Banana",
+  },
+];
 
 function App() {
+  const [newProduct, setNewProduct] = React.useState<{
+    product_name: string;
+    img_url: string;
+  }>({ product_name: "", img_url: "" });
+
+  const [showModal, setShowModal] = React.useState(false);
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleAddProduct = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Spica Storage </h1>
+      <ul className="product-list">
+        <li
+          onClick={(e) => setShowModal(true)}
+          style={{ cursor: "pointer" }}
+          key={"product._id"}
         >
-          Learn React
-        </a>
-      </header>
+          <big>+ Add New Product</big>
+        </li>
+        {products?.map((product) => (
+          <li key={product._id}>{product.product_name}</li>
+        ))}
+      </ul>
+      <Modal show={showModal} setShow={setShowModal} hideCloseButton={false}>
+        <ModalHeader>
+          <h2>New Product</h2>
+        </ModalHeader>
+        <ModalBody>
+          <div className="input-container">
+            <input
+              className="product-input"
+              name="product_name"
+              type="text"
+              placeholder="Enter new product name..."
+              value={newProduct?.product_name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setNewProduct({ ...newProduct, product_name: e.target.value })
+              }
+            />
+            <input type="file" onChange={onFileChange} />
+            {newProduct.img_url.length ? (
+              <img alt="not found" width={"250px"} src={newProduct.img_url} />
+            ) : null}
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="submit-button" onClick={handleAddProduct}>
+            Submit
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
